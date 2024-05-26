cd3e3bfb27415196b25d18d7b13e1d609f401257
Monserrat G <gmonse.cc@gmail.com>
2024-05-25 18:17:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// Obtener todos los elementos con la clase 'typewriter'
+let typewriterElements = document.querySelectorAll('.typewriter');
+
+// Textos para cada elemento
+let texts = [
+  'Desarrollo sitios web',
+  'Proyectos que he creado recientemente...'
+];
+
+// Iterar sobre cada elemento y aplicar el efecto Typewriter con su texto respectivo
+typewriterElements.forEach((element, index) => {
+  let typewriter = new Typewriter(element, {
+    loop: true,
+    delay: 75,
+  });
+
+  typewriter
+    .pauseFor(2500)
+    .typeString(`<span style="color: #7b2cbf;">${texts[index]}</span>`)
+    .pauseFor(200)
+    .deleteChars(texts[index].length)
+    .start();
+});
+
